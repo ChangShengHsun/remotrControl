49d965a25f5e6c92e654261492687031a0611ca4
ChangShengHsun <ic046262@gmail.com>
2025-07-27 22:47:58 +0800

    add synchonize function, still need to be tested

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
 import json
 import threading
-HOST = '192.168.1.83'
+import time
+HOST = '0.0.0.0'
 PORT = 5000
 clients = []
 clients_lock = threading.Lock()
@@ -9,6 +10,40 @@ clients_lock = threading.Lock()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#socket.socket()
 server.bind((HOST, PORT))#.bind(IPaddress, port)
 server.listen(20) #start to listen, .listen(max amount clients in line)
+
+def day_micro():
+    return int(time.time() % 86400 * 1_000_000)
+    #as a notice, if you test lightdance over midnight, you have to run it again.
+
+def time_sync(client, addr, retries = 3):
+    for attempt in range(retries):
+        success_count = 0
+        for i in range(5):# do time sync 5 times and get the average
+            try:
+                t_send = day_micro()
+                sync_msg = {"type": "sync",
+                            "t_send": t_send}
+                client.sendall((json.dumps(sync_msg) + '\n').encode()) 
+
+                client.settimeout(0.5)
+                data = client.recv(1024).decode()
+                response = json.loads(data)
+                if response.get("type") == "sync_ack":
+                    print(f"SERVER: got sync_ack #{i+1} from {addr}")
+                    success_count += 1
+                else:
+                    print((f"SERVER: unexpected response: {response}"))
+            except Exception as e:
+                print(f"SERVER: sync error with {addr}: {e}")
+                break
+        if success_count >= 5:
+            client.settimeout(None)
+            return True
+        else:
+            print(f"SERVER: retrying time sync with {addr} ({attempt+1}/{retries})")
+    print(f"SERVER: time sync with {addr} failed after {retries} attempts")
+    client.settimeout(None)
+    return False
 
 def receiveClient():
     while True:
@@ -21,6 +56,9 @@ def receiveClient():
             print(f"SERVER: Error {e}")
             break
 
+        print(f"SERVER: start time sync with {addr}")
+        threading.Thread(target = time_sync, args = (client, addr), daemon = True).start()
+        
 
 def parse_input(user_input):
     #split user_input and divide cmd as well as args
@@ -81,7 +119,8 @@ while True:
             with clients_lock:
                 for c in clients:
                     try:
-                        c.sendall(json_str.encode())
+                        c.sendall(json_str.encode())                        
+
                         print("SERVER: send json")
                     except Exception as e:
                         print(f"SERVER: failed to send due to {e}")
